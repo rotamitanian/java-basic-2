58a9d736ff645dee84d8faa3cf090e5bf2a922e2
rotamitanian <tamitania12@gmail.com>
2021-10-12 15:55:13 +0700

    first commit

---
+++ biodata.java
@@ -0,0 +1,17 @@
+public class biodata {
+    public static void main(String[] args) {
+
+        String first_name = "Rotami";
+        String last_name ="Tania Ninata";
+        int age = 20;
+        String phone_number = "135961659";
+        String address ="Kuala Lumpur";
+
+        System.out.println("First Name:" + first_name);
+        System.out.println("Last Name:" + last_name);
+        System.out.println("Age:" + age);
+        System.out.println("Phone Number:" + phone_number);
+        System.out.println("Address:" + address);
+        
+    }
+}
